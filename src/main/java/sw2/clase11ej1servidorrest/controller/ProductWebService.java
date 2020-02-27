116441400ae35bb1962c16dca37748eed5135923
Stuardo <stuardo.lucho@pucp.pe>
2020-02-27 04:32:43 -0500

    solucion ej1 servidor rest

--- src/main/java/sw2/clase11ej1servidorrest/controller/ProductWebService.java
+++ src/main/java/sw2/clase11ej1servidorrest/controller/ProductWebService.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sw2.clase11ej1servidorrest.repository.ProductRepository;
 
 @RestController
+@CrossOrigin
 public class ProductWebService {
 
     @Autowired
